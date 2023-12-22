41d4fc9ba0ab634421372ce5247958f4cf2fba7b
CRISTIAN ȚURCANU <cristian.turcanu1@stud.ubbcluj.ro>
2023-12-22 22:09:27 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Turcanu_Cristian_Lab11.Data;
+
+namespace Turcanu_Cristian_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
